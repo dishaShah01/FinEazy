787966c05db9a0a50d7559ef3d40ba834e02c232
tusharsarkar3 <tushar.sarkar@somaiya.edu>
2021-11-13 15:09:48 +0530

    model created

--- manager/models.py
+++ manager/models.py
@@ -2,8 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User,AbstractUser
 
 
+class Stocks(models.Model):
+    user = models.ForeignKey(User, default='', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    dob = models.DateField(auto_now_add=True)
+    total_coins_bought = models.PositiveIntegerField()
+    total_money_invested = models.PositiveIntegerField()
+    total_money_now = models.PositiveIntegerField()
 
-    
 #class Profile(models.Model):
     #user = models.OneToOneField(User,default='', on_delete=models.CASCADE)
 
