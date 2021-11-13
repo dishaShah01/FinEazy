c02b6d3b7ff19fe654cbab5e33826d081b3ed24f
tusharsarkar3 <tushar.sarkar@somaiya.edu>
2021-11-13 14:17:37 +0530

    data gathered

--- manager/models.py
+++ manager/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User,AbstractUser
 
 
+
     
 #class Profile(models.Model):
     #user = models.OneToOneField(User,default='', on_delete=models.CASCADE)
--- manager/views.py
+++ manager/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render,redirect
 from .models import *
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login,logout,authenticate
+import requests
+import pandas as pd
+import numpy as np
 from .forms import *
 from django.contrib import messages
+from .keys import ALPHAVANTAGE_API_KEY
 from django.contrib.auth.decorators import login_required
 from django.views.generic import (
     ListView,
@@ -16,6 +20,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 def home(request):
+    # put_historical_data()
     return render(request,'homepage.html')     
 
 def logoutUser(request):
@@ -27,14 +32,14 @@ def loginUser(request):
     if request.user.is_authenticated:
         return redirect('home')
     else:
-       if request.method=="POST":
-        name=request.POST.get('username')
-        pwd=request.POST.get('password')
-        user=authenticate(request,username=name,password=pwd)
-        if user is not None:
-            login(request,user)
-            return redirect('home')
-       return render(request,'login.html')
+        if request.method=="POST":
+            name=request.POST.get('username')
+            pwd=request.POST.get('password')
+            user=authenticate(request,username=name,password=pwd)
+            if user is not None:
+                login(request,user)
+                return redirect('home')
+        return render(request,'login.html')
 
 def registerUser(request):
     if request.method == 'POST':
@@ -55,3 +60,23 @@ def dashboard(request):
 
     
 
+def put_historical_data():
+    df = pd.read_csv("manager\digital_currency_list.csv")
+    codes = df.iloc[:,0].values
+    for code in codes:
+        df = pd.DataFrame(columns=["Date", "Open", "High", "Low", "Close", "Volume"])
+        url_crypto = f"https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=BTC&market=INR&apikey={ALPHAVANTAGE_API_KEY}"
+        r = requests.get(url_crypto)
+        data = r.json()
+        try:
+            counter = 0
+            for days, values in data.get("Time Series (Digital Currency Daily)").items():
+                li = [days]
+                li.extend([values['1a. open (INR)'], values['2a. high (INR)'], values['3a. low (INR)'],
+                           values['4a. close (INR)'], values['5. volume']])
+                df.loc[counter] = li
+                counter += 1
+            df.to_csv(r"manager\crypto_data" + "\\" + code+ ".csv",index=False)
+        except:
+            print(code)
+            pass
